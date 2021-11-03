3e70eec88e101b5d0c418b07365a395572243e93
Jonathan Miller <jon@jmill.dev>
2021-11-03 09:07:59 -0400

    Double click canvas to add cards

--- src/ui/IdeaClockView.tsx
+++ src/ui/IdeaClockView.tsx
@@ -4,6 +4,7 @@ import ReactFlow, {
   addEdge,
   ConnectionMode,
   Elements,
+  FlowElement,
 } from "react-flow-renderer";
 import IdeaClockPlugin from "../index";
 import { nodeTypes } from "src/types";
@@ -18,6 +19,7 @@ export default function IdeaClockView({
   const [reactflowInstance, setReactflowInstance] = useState(null);
   const [numNodes, setNumNodes] = useState("12");
   const [noteElements, setNoteElements] = useState<Elements>([]);
+  const [selectedNoteIndices, setSelectedNoteIndices] = useState<string[]>([]);
   const radius = 300;
 
   const randomNotesHandler = async (): Promise<void> => {
@@ -33,11 +35,15 @@ export default function IdeaClockView({
   };
 
   const postFillHandler = (notes: TFile[]): void => {
-    buildCircle(notes);
+    if (selectedNoteIndices && selectedNoteIndices.length > 0) {
+      rebuildCircle(notes);
+    } else {
+      buildCircle(notes);
+    }
   };
 
   const buildCircle = (notes: TFile[]) => {
-    const num = notes.length;
+    const num = parseInt(numNodes);
     const type = 1;
     const start = -90;
     const slice = (360 * type) / num;
@@ -65,10 +71,45 @@ export default function IdeaClockView({
     setNoteElements(items);
   };
 
+  const rebuildCircle = (notes: TFile[]) => {
+    const num = parseInt(numNodes);
+    const type = 1;
+    const start = -90;
+    const slice = (360 * type) / num;
+
+    const items: Elements = [];
+    let i;
+    for (i = 0; i < num; i++) {
+      const rotate = slice * i + start;
+      const str_i = i.toString();
+
+      if (selectedNoteIndices.includes(str_i)) {
+        items.push({
+          id: str_i,
+          data: {
+            label: notes[i].basename,
+            path: notes[i].path,
+            selected: true,
+          },
+          type: "noteNode",
+          position: {
+            x: Math.cos((rotate * Math.PI) / 180) * radius,
+            y: Math.sin((rotate * Math.PI) / 180) * radius,
+          },
+        });
+      } else {
+        items.push({
+          ...noteElements[i],
+        });
+      }
+    }
+    setNoteElements(items);
+  };
+
   const onSelectionChange = (selectedElements: Elements) => {
-    console.log("updateSelection", selectedElements);
     const selectedElementIds =
       selectedElements && selectedElements.map((element) => element.id);
+    setSelectedNoteIndices(selectedElementIds);
     const newElements = noteElements.map((element) => {
       let selected = false;
       if (selectedElementIds && selectedElementIds.includes(element.id)) {
@@ -82,7 +123,6 @@ export default function IdeaClockView({
         },
       };
     });
-    console.log("newElements", newElements);
     setNoteElements(newElements);
   };
 
@@ -90,7 +130,7 @@ export default function IdeaClockView({
     setNoteElements((els) => addEdge({ ...params }, els));
   }, []);
 
-  const onElementClick = (e: any, element: any) => {
+  const onElementClick = (e: any, element: FlowElement) => {
     const { data } = element;
     if (e.ctrlKey || e.metaKey) {
       plugin.app.workspace.openLinkText(data.path, "", true, false);
@@ -106,9 +146,39 @@ export default function IdeaClockView({
     [reactflowInstance]
   );
 
+  const onDoubleClick = useCallback(
+    (e: any) => {
+      console.log(e);
+      if (e.target.className == "react-flow__pane") {
+        const currentTargetRect = e.currentTarget.getBoundingClientRect();
+        const position = {
+          x: e.screenX - 0.5 * currentTargetRect.width - 50,
+          y: e.screenY - 0.5 * currentTargetRect.height + 115,
+        };
+        console.log(position);
+        const newElements = noteElements.map((e) => {
+          return { ...e };
+        });
+        console.log(newElements);
+        newElements.push({
+          id: `${noteElements.length}`,
+          data: {
+            label: "",
+            path: "",
+            selected: false,
+          },
+          type: "noteNode",
+          position,
+        });
+        setNoteElements(newElements);
+      }
+    },
+    [noteElements]
+  );
+
   useEffect(() => {
-    if (reactflowInstance && noteElements.length > 0) {
-      reactflowInstance.fitView();
+    if (reactflowInstance && noteElements && noteElements.length > 0) {
+      reactflowInstance.fitView({ padding: 0.1 });
     }
   }, [reactflowInstance, noteElements.length]);
 
@@ -130,6 +200,8 @@ export default function IdeaClockView({
             multiSelectionKeyCode={"Shift"}
             onSelectionChange={onSelectionChange}
             connectionMode={ConnectionMode.Loose}
+            onDoubleClick={onDoubleClick}
+            zoomOnDoubleClick={false}
           />
         )}
       </div>
